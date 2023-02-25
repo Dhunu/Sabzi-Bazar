d6482de31f3e4bab8ef5cb7a7e0f7f58efef079f
Abhinav <abhinavmourya22@gmail.com>
2023-02-25 15:32:24 +0530

    customer_services_done

--- vegetable_sales_app/src/main/java/com/masai/service/CustomerService.java
+++ vegetable_sales_app/src/main/java/com/masai/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.masai.service;
 
+import java.util.List;
+
 import com.masai.exceptions.CustomerException;
 import com.masai.model.Customer;
 
@@ -7,8 +9,14 @@ public interface CustomerService {
 	
 	    public Customer getCustomerByEmail(String email) throws CustomerException;
 	    
-	    public Customer createCustomer(Customer customer) throws CustomerException;
+	    public Customer addCustomer(Customer customer) throws CustomerException;
 	    
 	    public Customer updateCustomer(Customer customer, String key) throws CustomerException;
+	    
+	    public String removeCustomer(String email, String key) throws CustomerException;
+	    
+	    public Customer viewCustomer(String email, String key) throws CustomerException;
+	    
+	    public List<Customer> viewCustomerList(String key) throws CustomerException;
        
 }
